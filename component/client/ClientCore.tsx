a41c5619fb9defb267ed90d3e8c42a054555baeb
Zhou Zhou <zhouzchina@outlook.com>
2017-04-22 01:46:36 -0400

    Replace eslint with tslint

--- component/client/ClientCore.tsx
+++ component/client/ClientCore.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Grid, Row} from "react-bootstrap";
+
 import ClientChat from "./ClientChat";
 import ClientNavbar from "./ClientNavbar";
 import IActiveKeyState from "./IActiveKeyState";
@@ -9,10 +10,8 @@ interface IClientCoreComponents {
   setting: JSX.Element;
 }
 
-interface IClientCoreProps {}
-
-class ClientCore extends React.Component<IClientCoreProps, IActiveKeyState> {
-  components: IClientCoreComponents;
+class ClientCore extends React.Component<any, IActiveKeyState> {
+  private components: IClientCoreComponents;
 
   constructor(props) {
     super(props);
@@ -21,8 +20,8 @@ class ClientCore extends React.Component<IClientCoreProps, IActiveKeyState> {
     // TODO: add lifecycle to go properly set state based on persistant use.
     // TODO: setting components.
     this.components = {
-      "chat": <ClientChat />,
-      "setting": <h1> setting </h1>
+      chat: <ClientChat />,
+      setting: <h1> setting </h1>
     };
 
     // initialize the activeKey.
@@ -37,11 +36,11 @@ class ClientCore extends React.Component<IClientCoreProps, IActiveKeyState> {
   /*
     Callback that handles Navbar transitions.
   */
-  handleSelection(eventKey: string) {
+  public handleSelection(eventKey: string) {
     this.setState({activeKey: eventKey});
   }
 
-  render() {
+  public render() {
     // offset from the top to avoid Navbar hangovers.
     const coreStyle = {
       marginTop: "60px"
