5b8da66aacf53716eeb2645310628821f2777874
lamdav <dav_lam@ymail.com>
2017-04-23 14:54:33 -0400

    revising directory structure

---
+++ component/main.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ClientCore from "./client/core/ClientCore.jsx";
+
+ReactDOM.render(<ClientCore />, document.getElementById("root"));
