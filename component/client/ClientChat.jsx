7dff94ffe4e6c7c3d955c91987b9a8658d818704
Fred Zhang <fredzqm@gmail.com>
2017-04-23 04:00:23 -0400

    WIP

--- component/client/ClientChat.jsx
+++ component/client/ClientChat.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
 import {Panel} from "react-bootstrap";
 import ClientMessenger from "./ClientMessenger.jsx";
+import RoomService from "./RoomService";
 
 class ClientChat extends Component {
+
+  room: RoomService;
+
   constructor(props) {
     super(props);
+    this.state = {
+      history: []
+    };
+    this.room = new RoomService(props.url, (message) => {
+      this.state.history.push(message);
+    });
   }
 
   render() {
@@ -19,7 +29,11 @@ class ClientChat extends Component {
     return (
       <div>
         <Panel header="Chat" bsStyle="primary" footer={messenger}>
-          <div style={bodyStyle} />
+          <ul style={bodyStyle}>
+            {
+              states.history.map((message)=><li>message</li>)
+            }
+          </ul>
         </Panel>
       </div>
     );
---
+++ component/client/RoomService.js
@@ -0,0 +1,30 @@
+import * as firebase from "firebase";
+
+export default class RoomService {
+  ref : firebase.database.Reference
+
+  constructor(url, onMessagePosted) {
+    this.ref = firebase.database().ref(url);
+
+    this.ref.on('child_added', (data) => {
+      onMessagePosted(data.val());
+    });
+
+    this.ref.on('child_changed', (data) => {
+      throw "Messages should never changed";
+    });
+
+    this.ref.on('child_removed', function(data) {
+      throw "Messages should never be moved";
+    });
+  }
+
+  pushMessage(message: string) {
+    this.ref.push().set(message);
+  }
+
+  close() {
+    this.ref.close();
+  }
+
+}
