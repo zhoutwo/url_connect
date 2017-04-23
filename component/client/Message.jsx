44825a3b086fdc6c1b8f43acf0f7d4e6eb8ce107
lamdav <dav_lam@ymail.com>
2017-04-23 14:48:01 -0400

    adding Message component

---
+++ component/client/Message.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import {Grid, Row, Col, ListGroup, ListGroupItem}
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ListGroupItem bsStyle="info">
+        <Grid>
+          <Row>
+            <Col sm={4} md={4} >
+              {this.props.username}
+            </Col>
+            <Col sm={8} md={8} >
+              {this.props.message}
+            </Col>
+          </Row>
+        </Grid>
+      </ListGroupItem>
+    )
+  }
+}
+
+Message.propTypes = {
+  username: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired
+};
+
+export default Message;
