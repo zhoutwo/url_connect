68e37e958c79783688e52c0ba34dca3c123228ff
Zhou Zhou <zhouzchina@outlook.com>
2017-04-24 12:06:07 -0400

    Fix firebase errors; disable source-map for webpack; fix imported client code styling

--- component/main.tsx
+++ component/main.tsx
@@ -1,6 +1,18 @@
+import {initializeApp} from "firebase";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import ClientCore from "./client/ClientCore";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBakHeV8lMlysuBRtIWU9vz_hv6dF_zHxM",
+  authDomain: "url-connet.firebaseapp.com",
+  databaseURL: "https://url-connet.firebaseio.com",
+  projectId: "url-connet",
+  storageBucket: "url-connet.appspot.com",
+  messagingSenderId: "1089725560944"
+};
+initializeApp(config);
 
 ReactDOM.render(<ClientCore />, document.getElementById("root"));
