c88dbeb9772ddd02efddbbbb1c06c3758dd376bb
lamdav <dav_lam@ymail.com>
2017-04-24 19:08:53 -0400

    stylized chat components

--- component/client/chat/Message.tsx
+++ component/client/chat/Message.tsx
@@ -7,14 +7,52 @@ interface IMessageProps {
 }
 
 class Message extends React.Component<IMessageProps, any> {
-  constructor(props) {
+  constructor(props: IMessageProps) {
     super(props);
   }
 
-  public render() {
+  // Then we'll use the int and convert to hex.
+  // Source: https://www.designedbyaturtle.co.uk/2014/convert-string-to-hexidecimal-colour-with-javascript-vanilla/
+  private hashCode(str: string): number {
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+          hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      return hash;
+  }
+
+  // Convert an int to hexadecimal with a max length
+  // of six characters.
+  // Source: https://www.designedbyaturtle.co.uk/2014/convert-string-to-hexidecimal-colour-with-javascript-vanilla/
+  private intToARGB(i: number): string {
+      var hex = ((i >> 24) & 0xFF).toString(16) +
+              ((i >> 16) & 0xFF).toString(16) +
+              ((i >> 8) & 0xFF).toString(16) +
+              (i & 0xFF).toString(16);
+      // Sometimes the string returned will be too short so we
+      // add zeros to pad it out, which later get removed if
+      // the length is greater than six.
+      hex += '000000';
+      return hex.substring(0, 6);
+  }
+
+  private invertColor(hexColor) {
+    let color = hexColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
+
+  public render(): JSX.Element {
+    let backgroundColor = "#" + this.intToARGB(this.hashCode(this.props.username));
+    let fontColor = this.invertColor(backgroundColor);
     const messageStyle = {
-      marginBottom: "10px",
-      marginTop: "10px"
+      backgroundColor: backgroundColor,
+      color: fontColor
     };
 
     return (
