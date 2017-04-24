f4bb30e4810ad8b1b572d08af99ea54c1901bc9f
Zhou Zhou <zhouzchina@outlook.com>
2017-04-24 13:06:58 -0400

    Fix linter error

--- component/main.tsx
+++ component/main.tsx
@@ -9,9 +9,9 @@ const config = {
   apiKey: "AIzaSyBakHeV8lMlysuBRtIWU9vz_hv6dF_zHxM",
   authDomain: "url-connet.firebaseapp.com",
   databaseURL: "https://url-connet.firebaseio.com",
+  messagingSenderId: "1089725560944",
   projectId: "url-connet",
-  storageBucket: "url-connet.appspot.com",
-  messagingSenderId: "1089725560944"
+  storageBucket: "url-connet.appspot.com"
 };
 initializeApp(config);
 
