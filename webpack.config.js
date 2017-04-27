64fbd45a6195092777e3d2a7fcb2330a8e128ef4
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-04-27 14:22:16 -0400

    Remove unwanted characters from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,5 @@ module.exports = {
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
     ]
-  }co
+  }
 };
