0a014b60bd95d001df0b83dc5f0c85a585190be4
lamdav <dav_lam@ymail.com>
2017-04-30 23:12:09 -0400

    added background.tsx page and chrome tab read functionality

---
+++ component/background/background.tsx
@@ -0,0 +1,36 @@
+import * as firebase from "firebase";
+import ClientReducer from "../redux/reducer/ClientReducer";
+import {SWITCH_ROOM, switchRoom} from "../redux/action/Types";
+import {createStore, applyMiddleware} from "redux";
+import thunkMiddleware from "redux-thunk";
+import {wrapStore, alias} from 'react-chrome-redux';
+import {createLogger} from "redux-logger";
+import {PORT_NAME} from "../Constants";
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyBakHeV8lMlysuBRtIWU9vz_hv6dF_zHxM",
+  authDomain: "url-connet.firebaseapp.com",
+  databaseURL: "https://url-connet.firebaseio.com",
+  messagingSenderId: "1089725560944",
+  projectId: "url-connet",
+  storageBucket: "url-connet.appspot.com"
+};
+firebase.initializeApp(config);
+
+// React-Chrome-Redux specific: It intercepts an action by type and
+// forwards the request to the defined function.
+const aliases = {
+  [SWITCH_ROOM]: switchRoom
+}
+
+// Create Redux Store.
+const loggerMiddleware = createLogger();
+const middleware = applyMiddleware(alias(aliases), thunkMiddleware, loggerMiddleware);
+const store = createStore(ClientReducer, middleware);
+
+// Wrap Store with React-Chrome-Redux Proxy.
+wrapStore(store, {portName: PORT_NAME});
+
+// Log data initialization.
+console.log("[ INFO ] : Store initialized in background");
