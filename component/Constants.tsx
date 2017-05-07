71cb6819b42f7b6c1cf7f6419c94a1223c309101
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-07 12:26:36 -0400

    Add React-Router across the project

---
+++ component/Constants.tsx
@@ -0,0 +1,8 @@
+export const PORT_NAME = "url_connect_background";
+export const UNDEFINED_STATUS = "UNDEFINED_STATUS";
+
+export const CHAT_LINK = "/chat";
+export const SETTING_LINK = "/setting";
+
+export const NOOP_USERNAME = "alphaUser";
+export const NOOP_URL = "https://www.google.com/";
