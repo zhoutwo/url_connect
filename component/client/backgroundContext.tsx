0abcaf7fcb725f66cb4f3f393f6090f72bfa3707
Fred Zhang <fredzqm@gmail.com>
2017-05-09 22:25:01 -0400

    move unloadEventListener to backgroundContext

--- component/client/backgroundContext.tsx
+++ component/client/backgroundContext.tsx
@@ -1,4 +1,11 @@
 const backgroundContext: any = chrome.extension.getBackgroundPage();
 
+addEventListener("unload", (event) => {
+  const backgroundPage = chrome.extension.getBackgroundPage();
+  if (backgroundPage) {
+    room.close();
+  }
+}, true);
+
 export const storage = backgroundContext.BackgroundStorageService;
 export const room = backgroundContext.room;
