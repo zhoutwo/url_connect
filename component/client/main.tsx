595b8c23ca084edd0f55a12e9892a911030344a6
Zhou Zhou <zhouzchina@outlook.com>
2017-05-09 20:53:49 -0400

    Move settings to its own directory

--- component/client/main.tsx
+++ component/client/main.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Redirect, Route} from "react-router-dom";
 import ClientIndependentChatRoom from "./chat/ClientIndependentChatRoom";
 import * as Constants from "./Constants";
 import ClientCore from "./core/ClientCore";
-import ClientSetting from "./core/ClientSetting";
+import ClientSetting from "./settings/ClientSetting";
 
 ReactDOM.render((
   <Router>
