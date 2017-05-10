32c24d7a9414603ae3000bf6680971b95b698329
Fred Zhang <fredzqm@gmail.com>
2017-05-09 22:32:17 -0400

    fix linter issues

--- component/client/backgroundContext.tsx
+++ component/client/backgroundContext.tsx
@@ -1,11 +1,11 @@
 const backgroundContext: any = chrome.extension.getBackgroundPage();
 
+export const storage = backgroundContext.BackgroundStorageService;
+export const room = backgroundContext.room;
+
 addEventListener("unload", (event) => {
   const backgroundPage = chrome.extension.getBackgroundPage();
   if (backgroundPage) {
     room.close();
   }
 }, true);
-
-export const storage = backgroundContext.BackgroundStorageService;
-export const room = backgroundContext.room;
