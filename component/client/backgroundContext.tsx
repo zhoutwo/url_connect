bffa2b2fd06f13377c4b77ee92ffdbb9b207a38d
Fred Zhang <fredzqm@gmail.com>
2017-05-12 13:03:12 -0400

    simplify unload event listener

--- component/client/backgroundContext.tsx
+++ component/client/backgroundContext.tsx
@@ -4,8 +4,5 @@ export const storage = backgroundContext.backgroundStorageService;
 export const room = backgroundContext.room;
 
 addEventListener("unload", (event) => {
-  const backgroundPage = chrome.extension.getBackgroundPage();
-  if (backgroundPage) {
-    room.close();
-  }
+	room.close();
 }, true);
