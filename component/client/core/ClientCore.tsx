a7304299ac9ff41927cfcf1e40f0af13a219cc34
lamdav <dav_lam@ymail.com>
2017-05-13 21:33:58 -0400

    updated ClientCore to initalize data

--- component/client/core/ClientCore.tsx
+++ component/client/core/ClientCore.tsx
@@ -1,18 +1,40 @@
 import * as React from "react";
-import {Grid, Row} from "react-bootstrap";
+import {Button, Grid, Modal, ModalTitle, ModalBody, ModalFooter, ModalHeader, FormGroup, FormControl, Row} from "react-bootstrap";
 
+import {storage} from "../backgroundContext";
+import {STORAGE_KEY_INITIALIZED, STORAGE_KEY_USERNAME} from "../Constants";
 import ClientNavbar from "./ClientNavbar";
 import IActiveKeyState from "./IActiveKeyState";
 
-class ClientCore extends React.Component<any, IActiveKeyState> {
+interface ClientCoreState {
+  activeKey: string;
+  initialized: boolean;
+  username: string;
+}
+
+class ClientCore extends React.Component<any, ClientCoreState> {
   constructor(props: any) {
     super(props);
 
     // TODO: add lifecycle to go properly set state based on persistant use. (See below.)
     // initialize the activeKey.
     this.state = {
-      activeKey: "chat" // TODO: set based on redux store/persistant data?
+      activeKey: "chat", // TODO: set based on redux store/persistant data?
+      initialized: false,
+      username: ""
     };
+
+    // Only change initialize status if setting is found.
+    storage.get(STORAGE_KEY_INITIALIZED).then((hasInitalized) => {
+      if (hasInitalized) this.setState({initialized: hasInitalized});
+    });
+
+    storage.subscribe((data) => {
+      this.setState({initialized: data.initialized})
+    });
+
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleSave = this.handleSave.bind(this);
   }
 
   public render(): JSX.Element {
@@ -22,16 +44,54 @@ class ClientCore extends React.Component<any, IActiveKeyState> {
     };
 
     return (
-      <Grid>
-        <Row>
-          <ClientNavbar initialKey={this.state.activeKey}/>
-        </Row>
+      <div>
+        <Modal {... this.props} show={!this.state.initialized}>
+          <Modal.Header>
+            <Modal.Title>Setting Up url_connect</Modal.Title>
+          </Modal.Header>
 
-        <Row style={coreStyle}>
-          {this.props.children}
-        </Row>
-      </Grid>
+          <Modal.Body>
+            <form onSubmit={(event) => event.preventDefault()}>
+              <FormGroup>
+                <FormControl type="text"
+                             value={this.state.username}
+                             onChange={this.handleUsernameChange}
+                             placeholder="username" />
+              </FormGroup>
+            </form>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button bsStyle="success"
+                    block={true}
+                    disabled={this.state.username.length === 0}
+                    onClick={this.handleSave}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Grid>
+          <Row>
+            <ClientNavbar initialKey={this.state.activeKey}/>
+          </Row>
+
+          <Row style={coreStyle}>
+            {this.props.children}
+          </Row>
+       </Grid>
+     </div>
     );
+  }
+
+  private handleUsernameChange(event): void {
+    this.setState({username: event.target.value});
+  }
+
+  private handleSave(): void {
+    storage.set(STORAGE_KEY_USERNAME, this.state.username)
+      .then(storage.set(STORAGE_KEY_INITIALIZED, true)
+        .then(this.setState({initialized: true, username: ""})));
   }
 }
 
