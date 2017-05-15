6ac58ea4d325622326bd1ef0c0f58fa562a62022
Fred Zhang <fredzqm@gmail.com>
2017-05-15 15:52:47 -0400

    dynamically generate the drop down menu for private rooms

--- component/client/Constants.tsx
+++ component/client/Constants.tsx
@@ -3,6 +3,7 @@ export const UNDEFINED_STATUS = "UNDEFINED_STATUS";
 
 export const CHAT_LINK = "/chat";
 export const SETTING_LINK = "/setting";
+export const PRIVATE_CHAT = "/privateChat";
 
 export const NOOP_USERNAME = "alphaUser";
 export const NOOP_URL = "https://www.google.com/";
