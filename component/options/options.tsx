c67d2772e70085a7bb0014a8a29d738462b22d5d
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2017-05-17 23:21:57 -0400

    Migrate video-chat to an options page

---
+++ component/options/options.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import VideoChatContainer from "./video-chat/VideoChatContainer";
+
+ReactDOM.render(<VideoChatContainer/>, document.getElementById("root"));
